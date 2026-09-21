9c7acffe18c5af6fe37581aabbb614c2439fa158
thetandtstudios-maker <thetandtstudios@gmail.com>
2026-09-21 20:46:29 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Nav from "@/components/Nav";
+import Hero from "@/components/Hero";
+import Stats from "@/components/Stats";
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <main>
+        <Hero />
+        <Stats />
+      </main>
+    </>
+  );
+}
